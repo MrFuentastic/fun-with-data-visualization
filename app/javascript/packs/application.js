9441622a44767dffac93dc631b5a6b2047abe057
MrFuentastic <vfuentes8587@gmail.com>
2018-05-27 11:00:00 -0500

    included ajax request

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,21 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-console.log('Hello World from Webpacker')
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#app',
+    data: {
+      message: "Can you say hello?",
+      potholes: 
+    },
+    mounted: function() {
+      $.get("https://data.cityofchicago.org/resource/_311-potholes.json").success(
+        function(response) {
+          console.log(this);
+          // response.forEach(function(lead) {
+          //   lead.showEvents = false;
+          // });
+          // this.potholes = response;
+          // this.leads = _.orderBy(this.leads, ["most_recent_event"], ["desc"]);
+        }.bind(this)
+      );
+    }
+  })
+})
