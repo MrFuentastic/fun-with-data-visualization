c17e0d33d72ed2621b8f305039504d7fa6a7a06e
MrFuentastic <vfuentes8587@gmail.com>
2018-05-28 14:30:13 -0500

    added axios trying to get it to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,33 @@
+import Vue from 'vue/dist/vue.esm'
+import axios from 'axios'
+
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
       message: "Can you say hello?",
-      potholes: 
+      potholes: []
     },
     mounted: function() {
-      $.get("https://data.cityofchicago.org/resource/_311-potholes.json").success(
-        function(response) {
-          console.log(this);
+      // $.get("https://data.cityofchicago.org/resource/_311-potholes.json").success(
+      //   function(response) {
+      //     console.log(this);
           // response.forEach(function(lead) {
           //   lead.showEvents = false;
           // });
           // this.potholes = response;
           // this.leads = _.orderBy(this.leads, ["most_recent_event"], ["desc"]);
-        }.bind(this)
-      );
+        // }.bind(this)
+      // );
+      this.loading = true;
+      axios.get("https://data.cityofchicago.org/resource/_311-potholes.json")
+      .then((response)  =>  {
+        this.loading = false;
+        this.potholes = response.data.value;
+        console.log(this.potholes);
+      }, (error)  =>  {
+        this.loading = false;
+      })
     }
   })
 })
