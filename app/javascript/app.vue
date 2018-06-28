a316bd9e0c22eff4167cbe8fbfa68c81549b37ce
MrFuentastic <vfuentes8587@gmail.com>
2018-06-28 01:24:00 -0500

    separated stuff into components, wip

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,35 @@
 <template>
-  <div id="app">
+  <div>
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  import Vue from 'vue/dist/vue.esm';
+  import axios from 'axios';
+  import VueAxios from 'vue-axios';
+
+  Vue.use(VueAxios, axios)
+
+  export default {
+    data: function () {
+      return {
+        message: "asdfasdfasdfadsf",
+        potholes: []
+      }
+    },
+    mounted: function() {
+      axios.get("https://data.cityofchicago.org/resource/_311-potholes.json")
+      .then((response)  =>  {
+        this.potholes = response.data
+      })
     }
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+  p {
+    font-size: 20px;
+    text-align: center;
+  }
 </style>
---
+++ app/javascript/googlemap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <gmap-map
+      :center="center"
+      :zoom="12"
+      style="width:100%;  height: 400px;"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GoogleMap",
+    data: {
+      potholes: []
+    }
+  }
+</script>
